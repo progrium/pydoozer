8238ef9fd033fbbe2fe6ccbf2a584603ce6a0248
Allan Beaufour <allan@chartbeat.com>
2012-05-23 15:08:19 -0400

    version 0.1 -> 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='PyDoozer',
-    version='0.1.0',
+    version='0.2.0',
     author='Jeff Lindsay',
     author_email='jeff.lindsay@twilio.com',
     description='doozer client',
